828f0cb2f3eaa2224d75256b2b14b29b9d472f92
Emil Shalaumov <emil.shalaumov@gmail.com>
2019-09-23 21:00:30 +0300

    Initial commit - Base first HomeWork

---
+++ Test3/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test3
+//
+//  Created by Эмиль Шалаумов on 23/09/2019.
+//  Copyright © 2019 Emil Shalaumov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
